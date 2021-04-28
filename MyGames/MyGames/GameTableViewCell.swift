1c7961c7b237bae5c3736bf7104d16843d6da6fc
Douglas Frari <douglas.frari@gmail.com>
2021-04-28 08:09:57 -0300

    Conteúdos da aula 2.

---
+++ MyGames/MyGames/GameTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  GameTableViewCell.swift
+//  MyGames
+//
+//  Created by Douglas Frari on 4/27/21.
+//
+
+import UIKit
+
+// GAME no singular representa nossa CELULA
+class GameTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ivCover: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbConsole: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
