70a44d6547a16692c4ae24677e199d177644c627
Douglas Frari <douglas_delfrari@apple.com>
2021-04-29 09:07:11 -0300

    Aula 2 - melhorias

--- MyGames/MyGames/GamesTableViewController.swift
+++ MyGames/MyGames/GamesTableViewController.swift
@@ -6,43 +6,70 @@
 //
 
 import UIKit
+import CoreData
 
 // GAMES no plural representa nossa lista de jogos
 class GamesTableViewController: UITableViewController {
 
+    var fetchedResultController: NSFetchedResultsController<Game>!
+
+    lazy var label: UILabel = {
+        
+        let label = UILabel()
+        // mensagem default
+        label.text = "Você não tem jogos cadastrados"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        loadGames()
     }
     
+    func loadGames() {
+        // Coredata criou na classe model uma funcao para recuperar o fetch request
+        let fetchRequest: NSFetchRequest<Game> = Game.fetchRequest()
+             
+        // definindo criterio da ordenacao de como os dados serao entregues
+        let gameTitleSortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [gameTitleSortDescriptor]
+             
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultController.delegate = self
+             
+        do {
+            try fetchedResultController.performFetch()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let count = fetchedResultController?.fetchedObjects?.count ?? 0
+        tableView.backgroundView = count == 0 ? label : nil
+        return count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GameTableViewCell
+        
+        guard let game = fetchedResultController.fetchedObjects?[indexPath.row] else {
+          return cell
+        }
+                             
+        cell.prepare(with: game)
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -89,4 +116,24 @@ class GamesTableViewController: UITableViewController {
     }
     */
 
+} // fim de classe
+
+
+extension GamesTableViewController: NSFetchedResultsControllerDelegate {
+ 
+    // sempre que algum objeto for modificado esse metodo sera notificado
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+     
+        switch type {
+            case .delete:
+                if let indexPath = indexPath {
+                    // Delete the row from the data source
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+                break
+            default:
+                tableView.reloadData()
+        }
+    }
 }
+
